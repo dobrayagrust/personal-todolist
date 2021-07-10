4ca3b74d8f3fb48c568cf0f4fec8367affcc8d7c
dobrayagrust <rzhavoe.vremya@gmail.com>
2021-07-10 21:29:37 +0300

    create tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Todolist} from './Todolist';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const tasks = [
+        { id: 1, title: "1984", isDone: true },
+        { id: 2, title: "The Financier", isDone: true },
+        { id: 4, title: "The Stoic", isDone: true },
+        { id: 5, title: "The Titan", isDone: true },
+        { id: 6, title: "The Double", isDone: true },
+        { id: 7, title: "The Master and Margarita", isDone: false },
+    ]
+
+    return (
+        <div className="App">
+            <Todolist title="Reading list" tasks={tasks} />
+        </div>
+    );
 }
 
 export default App;
